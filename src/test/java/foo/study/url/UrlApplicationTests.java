bbe240964eba9963aff69ca6ef6eaede03f75741
foo(이준형) <lleellee013@gmail.com>
2021-03-07 21:28:58 +0900

    initialize project

---
+++ src/test/java/foo/study/url/UrlApplicationTests.java
@@ -0,0 +1,13 @@
+package foo.study.url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
